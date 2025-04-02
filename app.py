9f3fc2fa5ddbc0b193b9859ff1d9d304d5b1882c
Mallikarjun <jamadarkhani29.maj@gmail.com>
2025-04-02 18:10:42 +0000

    working image classifier

--- app.py
+++ app.py
@@ -66,13 +66,11 @@ def predict_image(image_path: str, model: torch.nn.Module) -> str:
     """
     # Define transformations for the image
     transform = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
 
     # Load the image
     image = Image.open(image_path).convert("RGB")
